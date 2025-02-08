1bec87269e00686daaccf438ca86b6db31e9547e
Krishnadevsudhinan <krishnadevsudhinan@gmail.com>
2025-02-08 18:09:31 +0000

    Add files via upload

---
+++ 1-DDL Commands 3.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE SCHOOL;
+DROP DATABASE SCHOOL;
+use SCHOOL;
+create table STUDENT (ROLLNO int PRIMARY KEY,NAME varchar(20),MARKS INT NOT NULL, GRADE char);
+INSERT INTO STUDENT VALUES (1, "Arun", 35, "B");
+Insert Into Student values (2, "Bijoy",42, "A");
+select * from student;
+alter table student add contact int not null;
+alter table student drop column grade;
+rename table student to CLASSTEN;
+DESC CLASSTEN;
+TRUNCATE TABLE CLASSTEN;
+select * from CLASSTEN;
